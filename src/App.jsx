011ed36faf0c3102ae9ee3939ad99b4c6b08c865
Mohammad Amaan <mdamaan00@gmail.com>
2022-07-19 15:44:48 +0530

    ver 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,20 @@ import Blogs from "./MyComponents/Blogs.jsx";
 import Footer from "./MyComponents/Footer.jsx";
 import SingleBlogPost from "./MyComponents/SingleBlogPost.jsx";
 import "./MyComponents/css/whatsapp.css";
-import AnimatedCursor from "react-animated-cursor";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <AnimatedCursor
-          className="cursor"
-          innerSize={8}
-          outerSize={40}
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={1.5}
-        />
         <link
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
         ></link>
         <a
           href="https://wa.me/+918080919144"
-          class="whatsapp_float fab fa-whatsapp"
+          className="whatsapp_float fab fa-whatsapp"
           target="_blank"
           rel="noopener noreferrer"
         >
